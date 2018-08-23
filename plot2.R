cb680a05a58b8aebaf31e35dc43516b34dabee41
sumit <sumitkumarsharma@gmail.com>
2018-08-23 10:15:50 +0530

    Latest Version

--- plot2.R
+++ plot2.R
@@ -6,8 +6,8 @@ if(!file.exists("household_power_consumption.txt")){unzip("c4p1.zip")}
 c4p1<-read.delim("household_power_consumption.txt", sep=";", header=TRUE)
 data<-c4p1[c4p1$Date=="1/2/2007"|c4p1$Date=="2/2/2007",]
 data$Global_active_power<-as.numeric(as.character(data$Global_active_power))
+data$DaTi<-as.POSIXct(paste(data$Date, data$Time, sep=" "), format= "%d/%m/%Y %H:%M:%S")
 library(gridExtra)
 png("plot2.png", width = 480, height= 480)
-plot(data$Global_active_power,type="l",xlab = "",ylab = "Global Active Power (kilowatts)",xaxt="n")
-axis(1,at=c(0,1441,2880),labels = c("Thu", "Fri", "Sat"))
+plot(data$DaTi,data$Global_active_power,type="l",xlab = "",ylab = "Global Active Power (kilowatts)")
 dev.off()
--- plot3.R
+++ plot3.R
@@ -5,14 +5,15 @@ if(!file.exists("c4p1.zip")){download.file(url, destfile = "c4p1.zip")}
 if(!file.exists("household_power_consumption.txt")){unzip("c4p1.zip")}
 c4p1<-read.delim("household_power_consumption.txt",sep=";",header=TRUE)
 data<-c4p1[c4p1$Date=="1/2/2007"|c4p1$Date=="2/2/2007",]
+data$DaTi<-as.POSIXct(paste(data$Date, data$Time, sep=" "), format= "%d/%m/%Y %H:%M:%S")
 data$Sub_metering_1<-as.numeric(as.character(data$Sub_metering_1))
 data$Sub_metering_2<-as.numeric(as.character(data$Sub_metering_2))
 data$Sub_metering_3<-as.numeric(as.character(data$Sub_metering_3))
 library(gridExtra)
 png("plot3.png", width = 480, height= 480)
-plot(data$Sub_metering_1,type="l",xlab = "",ylab = "Energy sub metering", xaxt="n")
-points(data$Sub_metering_2,type="l", col="red")
-points(data$Sub_metering_3,type="l", col="blue")
+plot(data$DaTi,data$Sub_metering_1,type="l",xlab = "",ylab = "Energy sub metering")
+points(data$DaTi, data$Sub_metering_2,type="l", col="red")
+points(data$DaTi, data$Sub_metering_3,type="l", col="blue")
 legend("topright", legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col=c("black", "red", "blue"), lty = 1,cex=1)
 dev.off()
 
